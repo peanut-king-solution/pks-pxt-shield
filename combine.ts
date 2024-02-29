1013e8e18ab773440027ff4dbdb395871c652d90
PeanutKingSolutionOffical <142392722+PeanutKingSolutionOffical@users.noreply.github.com>
2024-02-29 13:35:05 +0800

    Update combine.ts

--- combine.ts
+++ combine.ts
@@ -1025,27 +1025,38 @@ namespace pksdriver {
     //% blockId=light_lighton_intensity block="Light|%index|intensity|%intensity" subcategory="shield"
     //% intensity.min=0 intensity.max=4
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
-    export function LightOnIntesity(index: Motors, intensity: number): void {
+    export function LightOnIntesity(index: Motors, intensity: number, speed: number): void {
         if (!initialized) {
             initPCA9685()
         }
-        let intensity = 4
         if (intensity = 0) {
-            setPwm(0, 0, 0)
-        }
+            speed = 0
+        } 
         if (intensity = 1) {
-            setPwm(0, 0, 1024)  
+            speed = 1024 
         }
         if (intensity = 2) {
-            setPwm(0, 0, 2048)
+            speed = 2048
         }
         if (intensity = 3) {
-            setPwm(0, 0, 3072)
+            speed = 3072
         }
         if (intensity = 4) {
-            setPwm(0, 0, 4095)
+            speed = 4095
         }      
-
+        if (index > 4 || index <= 0)
+            return
+        let pn = (4 - index) * 2
+        let pp = (4 - index) * 2 + 1
+        if (speed >= 0) {
+            setPwm(pp, 0, speed)
+            setPwm(pn, 0, 0)
+        } else {
+            setPwm(pp, 0, 0)
+            setPwm(pn, 0, -speed)
+        }
+    }
+    
     //% weight=90
     //% blockId=light_lighton block="Light On|%index" subcategory="shield"
     export function LightOn(index: Motors): void {
